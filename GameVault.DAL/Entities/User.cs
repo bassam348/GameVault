66e89e337b9cb2766b87c5c1de5c33cc251a2a79
bassam348 <162562515+bassam348@users.noreply.github.com>
2025-07-28 16:32:30 +0200

    Update User.cs

--- GameVault.DAL/Entities/User.cs
+++ GameVault.DAL/Entities/User.cs
@@ -4,6 +4,11 @@ namespace GameVault.DAL.Entities
     public class User
     {
         public int User_ID {get; private set;}
+         public DateTime? CreatedOn { get; private set; }
+         public bool IsDeleted { get; private set; }
+         public DateTime? ModifiedOn { get; private set; }
+         public string CreatedBy { get; private set; }
+
         public List<Game>? Games {get; set;} = new List<Game>();
     }
 }
